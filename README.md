d71220b5deae7bcb342997bfc6dcf021e0ca669a
nebojsa.lalic <nebojsa.lalic@devtechgroup.com>
2017-02-22 16:54:39 +0100

    getters and setters update

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ PHP Repository for Learning Projects
 
 - 'git clone' project locally
 - all last changes are on 'develop' branch
-- in 'root' of your project RUN cmd, git bach or similar
+- in 'root' of your project RUN cmd, git bash or similar
 - then RUN command 'php -S localhost:8000'
 - after that you should be able to access application in your browser by typing 'localhost:8000' in adress bar
--- classes/user.php
+++ classes/user.php
@@ -9,6 +9,13 @@ class User
     private $email;
     private $job;
 
+    /**
+     * User constructor.
+     * @param $first_name
+     * @param $last_name
+     * @param $email
+     * @param $job
+     */
     public function __construct($first_name, $last_name, $email, $job)
     {
         $this->first_name = $first_name;
@@ -17,7 +24,74 @@ class User
         $this->job = $job;
     }
 
+    /**
+     * @return string
+     */
     public function getUserDetails(){
         return 'Name: ' . $this->first_name . ' ' . $this->last_name . ',<br>Email: ' . $this->email . '<br>Job: ' . $this->job;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+
+    /**
+     * @param mixed $first_name
+     */
+    public function setFirstName($first_name)
+    {
+        $this->first_name = $first_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+
+    /**
+     * @param mixed $last_name
+     */
+    public function setLastName($last_name)
+    {
+        $this->last_name = $last_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getJob()
+    {
+        return $this->job;
+    }
+
+    /**
+     * @param mixed $job
+     */
+    public function setJob($job)
+    {
+        $this->job = $job;
     }
 }
