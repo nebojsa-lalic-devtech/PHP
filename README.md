b8b11feca9b9aa0051098c374f0cd5689cca57a2
nebojsa.lalic <nebojsa.lalic@devtechgroup.com>
2017-02-22 16:38:19 +0100

    readme file - short update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # PHP
 PHP Repository for Learning Projects
+
+- 'git clone' project locally
+- all last changes are on 'develop' branch
+- in 'root' of your project RUN cmd, git bach or similar
+- then RUN command 'php -S localhost:8000'
+- after that you should be able to access application in your browser by typing 'localhost:8000' in adress bar
--- views/index.php
+++ views/index.php
@@ -4,7 +4,7 @@ $user = new User('Nebojsa', 'Lalic', 'nebojsa.lalic@devtechgroup.com', 'Software
 $user2 = new User('Petar', 'Petrovic', 'petarpetrovic@gmail.com', 'QA');
 $user3 = new User('X', 'Man', 'xman@yahoo.com', 'Project manager');
 
-echo "<h3>Hello from autoloaded class('User') with next details:</h3><br>";
+echo '<h3>Hello from autoloaded class("User") with next details:</h3><br>';
 echo '<p>' . $user->getUserDetails() . '</p>';
-echo "<br>" . '<p>' . $user2->getUserDetails() . '</p>';
-echo "<br>" . '<p>' . $user3->getUserDetails() . '</p>';
+echo '<br>' . '<p>' . $user2->getUserDetails() . '</p>';
+echo '<br>' . '<p>' . $user3->getUserDetails() . '</p>';
