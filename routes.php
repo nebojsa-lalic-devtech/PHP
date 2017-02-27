9671d15584261bd63e4d82426a0c069c933bbb47
nebojsa.lalic <nebojsa.lalic@devtechgroup.com>
2017-02-27 09:48:42 +0100

    Views folder moved

--- routes.php
+++ routes.php
@@ -5,19 +5,19 @@ $request_uri = explode('/', $_SERVER['REQUEST_URI'], 2);
 switch ($request_uri[1]) {
     // Home page
     case '':
-        require_once 'public/Views/index.php';
+        require_once 'app/NL/Views/index.php';
         break;
     // About page
     case 'about':
-        require_once 'public/Views/about.php';
+        require_once 'app/NL/Views/about.php';
         break;
     // Project page
     case 'project':
-        require_once 'public/Views/projects.php';
+        require_once 'app/NL/Views/projects.php';
         break;
     // Everything else
     default:
         header('HTTP/1.0 404 Not Found');
-        require_once 'public/Views/404.php';
+        require_once 'app/NL/Views/404.php';
         break;
 }
